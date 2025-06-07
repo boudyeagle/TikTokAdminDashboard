440b05c8b513d64492c761de5e27e97f50f30bc5
boudyeagle <your-email@example.com>
2025-06-07 17:29:29 +0300

    Rename README.md to show index.html on GitHub Pages

--- obj/Debug/net6.0-windows/TikTokAdminDashboard.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/TikTokAdminDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b579899b9352befe7304699267be190d8b69bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73e7f0eeb782dc5ed3fc2c2bb89e5a437d96124")]
 [assembly: System.Reflection.AssemblyProductAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
