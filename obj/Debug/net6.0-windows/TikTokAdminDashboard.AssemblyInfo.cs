fbb8d9b02d892c984a631be9596c6574b1114287
boudyeagle <your-email@example.com>
2025-06-11 22:45:32 +0300

    🎯 Initial upload of full one-page TikTok Admin Dashboard

--- obj/Debug/net6.0-windows/TikTokAdminDashboard.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/TikTokAdminDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa0fcc226ca12403543e2503a4bd1ec2185458c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bfb3a7a7909ed1e600e9c07a0d295fdc389021")]
 [assembly: System.Reflection.AssemblyProductAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TikTokAdminDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
